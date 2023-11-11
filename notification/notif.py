1c0782587c47626ac3d8d697c3072c7882e91c7e
ranadanesh <ranadanesh.79@gmail.com>
2023-11-11 09:03:04 +0330

    include router

---
+++ notification/notif.py
@@ -0,0 +1,13 @@
+import yagmail
+from notification.core import EMAIL_PASSWORD
+
+
+def send_notif(email, content):
+    try:
+
+        yag = yagmail.SMTP(user='ranadanesh.79@gmail.com', password=EMAIL_PASSWORD)
+        yag.send(to=email, subject='Testing Notification', contents=content)
+        print("Email sent successfully")
+    except Exception as e:
+        print("Error, email was not sent")
+        print(e)
